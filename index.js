81aa48460f5966bf7bf4b7502da1949bd641508c
Ajay Singh <helloajaysingh1@gmail.com>
2021-04-13 22:50:54 +0530

    added error handling middleware and user PUT req

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ require('dotenv').config()
 const User = require('./models/user')
 const app = express()
 
-app.use(cors())
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log('---------');
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---------')
+  next()
+}
 
-app.get('/', (request, response) => {
-  response.send("Hello there")
-})
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
+
+// app.get('/', (request, response) => {
+//   response.send("Hello there")
+// })
 
 app.get('/api/users', (request, response) => {
   User.find({}).then(users => {
@@ -17,37 +28,28 @@ app.get('/api/users', (request, response) => {
   })
 })
 
-app.get('/api/users/:id', (request, response) => {
+app.get('/api/users/:id', (request, response, next) => {
   const id = request.params.id
-  console.log(id)
   User.findById(id)
     .then(user => {
       user
         ? response.json(user)
         : response.status(404).end()
-    }).catch(error => {
-      response.status(400).send({error:'malformated id'}).end()
-    })
+    }).catch(error => next(error))
 })
 
-app.delete('/api/users/:id', (request, response) => {
+app.delete('/api/users/:id', (request, response, next) => {
   const id = request.params.id
   User.findByIdAndRemove(id)
     .then(user => {
-      user
-      ? response.json(user)
-      : response.status(404).end()
-    }).catch(error =>{
-      console.log(error);
-      response.status(400).send({error:'malformated id'}).end()
-    })
+      response.status(204).end()
+    }).catch(error => next(error))
 })
 
 app.post('/api/users/', (request, response) => {
   const user = request.body
 
   const newUser = new User({
-    "email": user.email,
     "fname": user.fname,
     "lname": user.lname,
     "password": user.password
@@ -62,11 +64,39 @@ app.post('/api/users/', (request, response) => {
     })
 })
 
+app.put('/api/users/:id', (request, response, next) => {
+  const user = request.body
+
+  const updatedUserData = {
+    "fname": user.fname,
+    "lname": user.lname,
+    "password": user.password
+  }
+
+  // bydefault updatedUser returns old res, optional param "{new : true}" fixes it, returns updated data
+  User.findByIdAndUpdate(request.params.id, updatedUserData, {new : true})
+    .then(updatedUser => {
+      response.json(updatedUser)
+    }).catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
